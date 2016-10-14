4474b82f95cdce3f180bda1afee097584438b2d5
Chenshijia <chenshijia@watermesprinter.fios-router.home>
2016-10-14 03:36:19 -0400

    Initial commit

---
+++ app/src/main/java/com/example/chenshijia/mybuttonapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.chenshijia.mybuttonapp;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView t1;
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.button);
+        t1 = (TextView)findViewById(R.id.textView);
+
+        b1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                t1.setText("Ouch, you clicked me!");
+                t1.setBackgroundColor(Color.MAGENTA);
+            }
+
+        });
+
+
+    }
+}
